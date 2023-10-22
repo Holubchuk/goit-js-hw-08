7652fcb4705d810ec1648838cbf19d0ba6e4c516
Holubchuk <sashagolubchuk3@gmail.com>
2023-10-22 17:24:50 -0400

    Task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+const { email, message } = form.elements;
+
+form.addEventListener('input', throttle(safeToStorage, 500));
+form.addEventListener('submit', handleSubmit);
+
+function safeToStorage() {
+  const data = {
+    email: email.value,
+    message: message.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+};
+
+if (localStorage.getItem('feedback-form-state')) {
+  email.value = savedData.email || "";
+  message.value = savedData.message || "";
+};
+
+function handleSubmit(event) {
+  event.preventDefault();
+  localStorage.clear();
+  console.log(savedData);
+  form.reset();
+};
