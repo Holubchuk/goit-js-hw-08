2436cc0dc34d48d4fce7399bd92dfacbd84c9129
Holubchuk <sashagolubchuk3@gmail.com>
2023-10-22 12:22:58 -0400

    Task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,11 +1,8 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
-console.log(galleryItems);
-
-import SimpleLightbox from "simplelightbox";
-import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
 
 const list = document.querySelector(".gallery");
 
